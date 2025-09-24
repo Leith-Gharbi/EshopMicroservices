7b2fbf556b2896b269ada65e30950b0e8d65e928
Leith <leithgharbi@yahoo.fr>
2025-09-24 14:35:40 +0200

    feat: add create product handler and HTTP post endpoint with Minimal API and Carter

--- src/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,6 +1,30 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿
+namespace Catalog.API.Products.CreateProduct;
+
+
+record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+record  CreateProductResponse(Guid Id);
+public class CreateProductEndpoint : ICarterModule
 {
-    public class CreateProductEndpoint
+    public void AddRoutes(IEndpointRouteBuilder app)
     {
+        app.MapPost("/products",async (CreateProductRequest request , ISender sender) =>
+        {
+         var command = request.Adapt<CreateProductCommand>();
+         var result = await sender.Send(command);
+         var response = result.Adapt<CreateProductResponse>();
+         return Results.Created($"/products/{response.Id}",response);
+
+
+        })
+            .WithName("CreateProduct")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create a new product")
+            .WithDescription("Creates a new product with the provided details.")
+            .WithTags("Products");
+
     }
 }
+
+ 
--- src/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,17 +1,35 @@
-﻿using MediatR;
+﻿
+
+using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using System.Xml.Linq;
 
 namespace Catalog.API.Products.CreateProduct;
 
 
-
-public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price):IRequest<CreateProductResult>;
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    :ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
-internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
         // Bussiness logic to create a product
-        throw new NotImplementedException();
+
+        // cretae Product enity from command object 
+        Product product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+        // TODO:
+        // Save to DB
+        // return CreateProductResult with new product id
+
+        return new CreateProductResult(Guid.NewGuid());
     }
 }
 
