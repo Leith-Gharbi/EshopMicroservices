b6edf036785235848b9457a50f49d4b7a4881126
Leith <leithgharbi@yahoo.fr>
2025-09-25 16:17:39 +0200

    feat: add get products endpoint

---
+++ src/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace Catalog.API.Products.GetProducts;
+
+
+public record GetProductsQuery() : IQuery<GetProductsResult>;
+public record GetProductsResult(IEnumerable<Product> Products);
+
+internal class GetProductsQueryHandler(IDocumentSession session ,ILogger<GetProductsQueryHandler> logger)
+    : IQueryHandler<GetProductsQuery, GetProductsResult>
+{
+    public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+    {
+
+       logger.LogInformation("GetProductsQueryHandler.Handle called with {@Query}" , query);
+
+        // Business logic to get products
+        var products = await session.Query<Product>().ToListAsync(cancellationToken);
+        logger.LogInformation("Retrieved {Count} products", products.Count);
+        return new GetProductsResult(products);
+    }
+
+}
+
