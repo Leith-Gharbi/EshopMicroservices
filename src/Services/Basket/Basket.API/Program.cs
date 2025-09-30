a7f527cff13b7a079a40f0a171dbea7f7ba71c10
Leith <leithgharbi@yahoo.fr>
2025-09-30 14:38:41 +0200

    feat: register and configure Marten DocumentDB library into Program.cs

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -2,11 +2,19 @@
 
 
 
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 var assembly = typeof(Program).Assembly; // Get the current assembly ( Basket.API )
 #region Add services to the container.
 
 builder.Services.AddCarter();  // register Carter in the DI container
+
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration.GetConnectionString("Database")!);
+    options.Schema.For<ShoppingCart>().Identity(x => x.UserName); // Register the ShoppingCart document ( pour que Marten sache comment stocker et récupérer les objets ShoppingCart )
+}).UseLightweightSessions();
 
 builder.Services.AddMediatR(config =>
 {
