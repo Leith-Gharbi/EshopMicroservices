ea25cbf14b35abcbc8ded5d452c0e469e44b9c51
Leith <leithgharbi@yahoo.fr>
2025-10-08 15:35:19 +0200

    feat: create clean architecture project for ordering microservices

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -49,6 +49,14 @@ builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
 builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
 {
     options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
+})
+    .ConfigurePrimaryHttpMessageHandler(() =>
+{
+    var handler = new HttpClientHandler
+    {
+        ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+    };
+    return handler;
 });
 
 #endregion
---
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿namespace Ordering.API
+{
+    public static class DependencyInjection
+    {
+
+        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        {
+
+            //services.AddCarter();
+            return services;
+        }
+
+
+        public static WebApplication useApiServices(this WebApplication app)
+        {
+
+
+            //app.MapCarter();
+
+            return app;
+        }
+    }
+}
