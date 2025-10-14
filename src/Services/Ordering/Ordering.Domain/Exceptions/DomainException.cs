87716c1065a78d804dc1c5ab5e67642629330c3c
Leith <leithgharbi@yahoo.fr>
2025-10-14 22:37:09 +0200

    feat: Develop DDD Rich-domain model for Entities

---
+++ src/Services/Ordering/Ordering.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Ordering.Domain.Exceptions;
+
+    public class DomainException :Exception
+    {
+        public string Details { get; set; } = default!;
+
+        public DomainException(string message) : base($"Doamin Exception : \"{message}\" throws from domain layer." ) { }
+
+    }
+   
