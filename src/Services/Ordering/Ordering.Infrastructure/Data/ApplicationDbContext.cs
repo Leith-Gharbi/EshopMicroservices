16f280fef0d048a391e9b1632fdd2b813592538a
Leith <leithgharbi@yahoo.fr>
2025-10-16 16:14:44 +0200

    feat: develop IApplicationDbContext for injection EFCore DB Context object in Ordering

--- src/Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContext.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
 ﻿
+using Ordering.Application.Data;
 using Ordering.Domain.Models;
 using System.Reflection;
 
 namespace Ordering.Infrastructure.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : DbContext , IApplicationDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
@@ -28,6 +29,7 @@ namespace Ordering.Infrastructure.Data
             builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
             base.OnModelCreating(builder);
         }
+
 
     }
 }
