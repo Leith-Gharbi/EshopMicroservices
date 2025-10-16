19db1ccc9842081733392645401eaf9236f01714
Leith <leithgharbi@yahoo.fr>
2025-10-16 22:49:02 +0200

    feat : Develop Domain Event Handlers with MediatR INotificationHandler in Ordering.App

---
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/OrderCreatedEventHandler.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+using Ordering.Domain.Abstractions;
+
+namespace Ordering.Application.Orders.EventHandlers
+{
+    public class OrderCreatedEventHandler(ILogger<OrderCreatedEventHandler> logger) : INotificationHandler<OrderCreatedEvent>
+    {
+        public Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Domain Event handled: {DomainEvent}", domainEvent.GetType().Name);
+            return Task.CompletedTask;
+        }
+    }
+}
