d673bd71819462e75a6a1992b8b42b99df8ca9ae
Leith <leithgharbi@yahoo.fr>
2025-10-16 17:11:29 +0200

    feat: develop Orders UpdateOrderCommand and Handler using IApplicationDbContext

---
+++ src/Services/Ordering/Ordering.Application/Exceptions/OrderNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿
+using BuildingBlocks.Exceptions;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Xml.Linq;
+
+namespace Ordering.Application.Exceptions
+{
+    public class OrderNotFoundException :  NotFoundException
+    {
+        public OrderNotFoundException(Guid id) : base("Order", id)
+        {
+        }
+
+
+    }
+}
