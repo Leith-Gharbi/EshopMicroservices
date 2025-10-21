0d4e44aec87a5db014cdd0704fcf89e43a045e18
Leith <leithgharbi@yahoo.fr>
2025-10-21 11:30:29 +0200

    feat: add healthcheck endpoint for  ordering microservices

--- src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -1,16 +1,18 @@
 ﻿using BuildingBlocks.Exceptions.Handler;
-using Carter;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 namespace Ordering.API
 {
     public static class DependencyInjection
     {
 
-        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        public static IServiceCollection AddApiServices(this IServiceCollection services , IConfiguration configuration)
         {
 
             services.AddCarter();
             services.AddExceptionHandler<CustomExceptionHandler>();
+            services.AddHealthChecks().AddSqlServer(configuration.GetConnectionString("Database"));
             return services;
         }
 
@@ -21,7 +23,10 @@ namespace Ordering.API
 
             app.MapCarter();
             app.UseExceptionHandler(opt => { }); // Use the exception handler middleware ( pour gérer les exceptions globalement) [ doit etre ajouté pour que AddExceptionHandler fonctionne (Appelle l’IExceptionHandler que tu as enregistré (CustomExceptionHandler dans ton cas))  ]
-
+            app.MapHealthChecks("/health", new HealthCheckOptions
+            {
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
             return app;
         }
     }
